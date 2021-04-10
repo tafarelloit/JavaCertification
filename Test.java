94a407dd013c29d81d13395130597c47d3d59328
tafarelloit <tafarelloit@gmail.com>
2021-04-10 09:19:09 -0300

    Expressões aritimeticas

---
+++ Test.java
@@ -0,0 +1,73 @@
+public class Test {
+    public static void main(String[] args) {
+
+        System.out.print("1+2*3+7*2%5 = ");
+        System.out.println(1+2*3+7*2%5);
+        System.out.print("1+(2*3)+((7*2)%5) = ");
+        System.out.println(1+(2*3)+((7*2)%5));
+
+        System.out.print("\n2 + 3 * 4 - 6 = ");
+        System.out.println(2 + 3 * 4 - 6);
+        System.out.print("(2 + (3 * 4)) - 6 = ");
+        System.out.println((2 + (3 * 4)) - 6);
+
+        System.out.print("\n14 / 7 * 2 + 30 / 5 + 1 = ");
+        System.out.println(14 / 7 * 2 + 30 / 5 + 1);
+        System.out.print("((14 / 7) * 2) + ((30 / 5) + 1) = ");
+        System.out.println(((14 / 7) * 2) + ((30 / 5) + 1));
+
+        System.out.print("\n(12 + 3) / 4 * 2 = ");
+        System.out.println((12 + 3) / 4 * 2);
+        System.out.print("((12 + 3) / 4) * 2 = ");
+        System.out.println(((12 + 3) / 4) * 2);
+
+        System.out.print("\n(238 % 10 + 3) % 7 = ");
+        System.out.println((238 % 10 + 3) % 7);
+        System.out.print("((238 % 10) + 3) % 7 = ");
+        System.out.println(((238 % 10) + 3) % 7);
+
+        System.out.print("\n(18 - 7) * (43 % 10) = ");
+        System.out.println((18 - 7) * (43 % 10));
+
+        System.out.print("\n2 + 19 % 5 - (11 * (5 / 2)) = ");
+        System.out.println(2 + 19 % 5 - (11 * (5 / 2)));
+        System.out.print("(2 + (19 % 5 )) - (11 * (5 / 2)) = ");
+        System.out.println((2 + (19 % 5 )) - (11 * (5 / 2)));
+
+        System.out.print("\n813 % 100 / 3 + 2.4 = ");
+        System.out.println(813 % 100 / 3 + 2.4);
+
+        System.out.print("((813 % 100) / 3) + 2.4 = ");
+        System.out.println(((813 % 100) / 3) + 2.4);
+
+        System.out.print("\n26 % 10 % 4 * 3 = ");
+        System.out.println(26 % 10 % 4 * 3);
+        System.out.print("((26 % 10) % 4) * 3 = ");
+        System.out.println(((26 % 10) % 4) * 3);
+
+        System.out.println(22 + 4 * 2);
+        System.out.println(22 + (4 * 2));
+
+        System.out.println(23 % 8 % 3);
+        System.out.println((23 % 8) % 3);
+
+
+        System.out.println(12 - 2 - 3);
+        System.out.println((12 - 2) - 3);
+
+        System.out.println(6/2 + 7/3);
+        System.out.println((6/2) + (7/3));
+
+        System.out.println(6 * 7 % 4);
+        System.out.println(6 * (7 % 4));
+
+        System.out.println(3 * 4 + 2 * 3);
+        System.out.println(177 % 100 % 10 / 2);
+        System.out.println(89 % (5 + 5) % 5);
+        System.out.println(392 / 10 % 10 / 2);
+        System.out.println(8 * 2 - 7 / 4);
+        System.out.println(37 % 20 % 3 * 4);
+        System.out.println(17 % 10 / 4);
+
+    }
+}
